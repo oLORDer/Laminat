0736ed536ab982112751232b0366b482a5770e59
oLORDer <justsendmeow@gmail.com>
2021-01-31 19:42:36 +0100

    Bodia ne slivai katki

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,43 +1,55 @@
-const tabs = document.querySelectorAll('.catalog__tab-head__item');
-const contents = document.querySelectorAll('.catalog__tab-content__item');
-const tabDiscounts = document.querySelectorAll('.discount__tab-head__item');
-const contentDiscounts = document.querySelectorAll('.discount__tab-content__item');
+const catalogMore = document.getElementById('catalogMore');
+const test = document.getElementById('test');
+const catalogContent = document.querySelectorAll('.catalog__content');
+const gold = document.getElementsByClassName('color-gold')
 
 
-tabs.forEach((tab, index) => {
-    tab.onclick = () => {
-        changeTab(index)
-    };
-});
+catalogMore.onclick = moreCatalog;
 
-function changeTab(index) {
-    for (let tab of tabs) {
-        tab.classList.remove('active');
-    }
-    tabs[index].classList.add('active');
 
-    for (let el of contents) {
-        el.classList.remove('active');
-    }
-    contents[index].classList.add('active');
+function moreCatalog() {
+    test.add(gold)
 }
 
+// catalogMore.onclick = function() {
+//     alert('oh no no no');  
+// } 
 
 
-tabDiscounts.forEach((tabDiscount, DisNumb) => {
-    tabDiscount.onclick = () => {
-        changeTabDis(DisNumb)
-    };
-});
+// catalogMore.onclick = function() {
+//     document.querySelectorAll('catalogMore');
+//     classList.remove('catalog-hiden')
+//     // catalogContent.classList.remove('catalog-hiden');
+//   }
 
-function changeTabDis(DisNumb) {
-    for (let tabDiscount of tabDiscounts) {
-        tabDiscount.classList.remove('discountOn');
-    }
-    tabDiscounts[DisNumb].classList.add('discountOn');
 
-    for (let elD of contentDiscounts) {
-        elD.classList.remove('discountOn');
-    }
-    contentDiscounts[DisNumb].classList.add('discountOn');
-}
+// for (let i = 0; i < catalogMore.length; i++) {
+//     catalogMore[i].addEventListener('catalogContent', function() {
+//         this.classList.remove;
+//     })
+// }
+
+
+
+
+
+
+
+// tabs.forEach((tab, index) => {
+//     tab.onclick = () => {
+//         changeTab(index)
+//     };
+// });
+
+// function changeTab(index) {
+//     for (let tab of tabs) {
+//         tab.classList.remove('active');
+//     }
+//     tabs[index].classList.add('active');
+
+//     for (let el of contents) {
+//         el.classList.remove('active');
+//     }
+//     contents[index].classList.add('active');
+// }
+
