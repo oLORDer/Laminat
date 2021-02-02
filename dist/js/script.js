a72fc2bd0a5d6fb15efd71c9dd5040ed315e8c30
oLORDer <justsendmeow@gmail.com>
2021-02-02 01:53:43 +0100

    ia jego wzjebal

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,55 +1,18 @@
 const catalogMore = document.getElementById('catalogMore');
-const test = document.getElementById('test');
-const catalogContent = document.querySelectorAll('.catalog__content');
-const gold = document.getElementsByClassName('color-gold')
+const hidden1 = document.getElementById('catalog-hiden1');
+const hidden2 = document.getElementById('catalog-hiden2');
 
-
-catalogMore.onclick = moreCatalog;
-
-
-function moreCatalog() {
-    test.add(gold)
+catalogMore.onclick = collapse;
+function collapse() {
+    hidden1.classList.remove('display-none');
+    hidden2.classList.remove('display-none');
+    catalogMore.classList.add('display-none');
 }
 
-// catalogMore.onclick = function() {
-//     alert('oh no no no');  
-// } 
 
-
-// catalogMore.onclick = function() {
-//     document.querySelectorAll('catalogMore');
-//     classList.remove('catalog-hiden')
-//     // catalogContent.classList.remove('catalog-hiden');
-//   }
-
-
-// for (let i = 0; i < catalogMore.length; i++) {
-//     catalogMore[i].addEventListener('catalogContent', function() {
-//         this.classList.remove;
-//     })
-// }
-
-
-
-
-
-
-
-// tabs.forEach((tab, index) => {
-//     tab.onclick = () => {
-//         changeTab(index)
-//     };
-// });
-
-// function changeTab(index) {
-//     for (let tab of tabs) {
-//         tab.classList.remove('active');
-//     }
-//     tabs[index].classList.add('active');
-
-//     for (let el of contents) {
-//         el.classList.remove('active');
-//     }
-//     contents[index].classList.add('active');
-// }
-
+$('[data-modal=call]').on('click', function() {
+    $('.overlay, #call').fadeIn('slow');
+})
+$('.modal__close').on('click', function() {
+    $('.overlay').fadeOut('slow');
+})
